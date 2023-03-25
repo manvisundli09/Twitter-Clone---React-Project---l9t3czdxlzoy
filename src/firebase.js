fd98c542d2bd223fd203cdff62904d92b329ff5f
manvisundli09 <116944171+manvisundli09@users.noreply.github.com>
2023-03-25 23:52:13 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,8 +8,8 @@ const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: "twitter-clone-nahar",
-    storageBucket: "twitter-clone-nahar.appspot.com",
+    projectId: "twitter-clone",
+    storageBucket: "twitter-clone.appspot.com",
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
 };
